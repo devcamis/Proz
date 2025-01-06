08a90f38a7bd00e22e808e71dc49e8ac99f95eba
Camila Ribeiro <camilamribeeiro@gmail.com>
2025-01-05 22:34:01 -0300

    fix - colocando tudo dentro de funcao

--- operadores.py
+++ operadores.py
@@ -1,14 +1,14 @@
-x = 2
-y = 4
-operador = "soma"
-print(operador,":")
-if operador == "soma":
-    print(x + y)
-elif operador == "subtracao":
-    print(x - y)
-elif operador == "multiplicacao":
-    print(x * y)
-elif operador == "divisao":
-    print(x / y)
-else:
-    print(0)
+def calc(x, y, operador):
+    if operador == "soma":
+        return(x + y)
+    elif operador == "subtracao":
+        return(x - y)
+    elif operador == "multiplicacao":
+        return(x * y)
+    elif operador == "divisao":
+        return(x / y)
+    else:
+        return(0)
+
+resultado = calc(4, 7, "soma")
+print(resultado)
