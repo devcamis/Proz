e8bc69d50d346bb87840fc39c4fec5c03d2980c8
Camila Ribeiro <camilamribeeiro@gmail.com>
2025-01-05 22:26:42 -0300

    Create operadores.py

---
+++ operadores.py
@@ -0,0 +1,14 @@
+x = 2
+y = 4
+operador = "soma"
+print(operador,":")
+if operador == "soma":
+    print(x + y)
+elif operador == "subtracao":
+    print(x - y)
+elif operador == "multiplicacao":
+    print(x * y)
+elif operador == "divisao":
+    print(x / y)
+else:
+    print(0)
