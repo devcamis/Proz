7e4a2f5458e74640698d3f6575972d69ce6d9685
Camila Ribeiro <camilamribeeiro@gmail.com>
2025-01-06 22:50:58 -0300

    Update calculadora_inteligente.py

--- calculadora_inteligente.py
+++ calculadora_inteligente.py
@@ -10,37 +10,40 @@ Menu principal:
 
 ''')
 
-codigo = input("Digite o código da operação desejada: ")
-num1 = input("Digite um número inteiro: ")
-num1 = int(num1)
-num2 = input("Digite outro número inteiro: ")
-num2 = int(num2)
+def somar_numeros(num1, num2):
+  print("O resultado é: ", num1 + num2)
+
+def subtrair_numero(num1, num2):
+  print("O resultado é: ", num1 - num2)
+
+def multiplicar_numero(num1, num2):
+  print("O resultado é: ", num1 * num2)
+
+def dividir_numero(num1, num2):
+  print("O resultado é: ", num1 / num2)
 
 programa_rodando = True
 
-def somar_numeros(num1, num2):
-  print (num1 + num2)
-  return
-def subtrair_numero(num1, num2):
-  return num1 - num2
-
-def multiplicar_numero(num1, num2):
-  return num1 * num2
-
-def dividir_numero(num1, num2):
-  return num1 / num2
-
 while programa_rodando:
+  codigo = input("Digite o código da operação desejada: ")
+
+  if codigo >= "5":
+    print("Opção inválida")
+    continue
+
+  elif codigo in ["1", "2", "3", "4"]:
+
+    num1 = int(input("Digite um número inteiro: "))
+    num2 = int(input("Digite outro número inteiro: "))
 
   if codigo == "1":
     somar_numeros(num1, num2)
-    programa_rodando = False
   elif codigo == "2":
-    subtrair_numero()
+    subtrair_numero(num1, num2)
   elif codigo == "3":
-    multiplicar_numero()
+    multiplicar_numero(num1, num2)
   elif codigo == "4":
-    dividir_numero()
+    dividir_numero(num1, num2)
   elif codigo == "0":
     print("Encerrando programa... Até logo!")
     programa_rodando = False
